f9d84703418d33b769bae0c30ea3493857c4f9ff
Bram <amogus@sus.com>
2022-01-12 17:36:51 +0100

    Update helper.ts with more packages + fix crypto requirement + export more effeciently

--- src/helper.ts
+++ src/helper.ts
@@ -1,21 +1,34 @@
-const config = require("../config.js");
+// Get variables from config.json
+import { config } from "../config.js";
 
-module.exports.base64decode = (message: string) => {
+// Import packages
+import Database from 'better-sqlite3'
+const db = new Database(config.database.main.url);
+const salt_db = new Database(config.database.salt.url);
+import forge from 'node-forge';
+import Memcached from "memcached";
+const memcached = new Memcached(`${config.memcached.url}:${config.memcached.port}`);
+
+import crypto from "crypto";
+
+function base64decode(message: string): string {
     return Buffer.from(message, "base64").toString("utf-8");
 }
 
-module.exports.base64encode = (message: string) => {
+function base64encode(message: string): string {
     return Buffer.from(message, "utf-8").toString("base64");
 }
 
-module.exports.pbkdf2 = (password: string, salt: string) => {
-    return require("crypto").pbkdf2Sync(password, salt, config.pbkdf2.iterations, config.pbkdf2.keyLength, config.pbkdf2.algorithm).toString("hex");
+function pbkdf2(password: string, salt: string): string {
+    return crypto.pbkdf2Sync(password, salt, config.pbkdf2.iterations, config.pbkdf2.keyLength, config.pbkdf2.algorithm).toString("hex");
 }
 
-module.exports.generateSalt = (passwordLength: number) => {
-    return require("crypto").randomBytes(passwordLength + (Math.floor((Math.random() * 30) + 10)) / 2).toString("hex");
+function generateSalt(passwordLength: number): string {
+    return crypto.randomBytes(passwordLength + (Math.floor((Math.random() * 30) + 10)) / 2).toString("hex");
 }
 
-module.exports.generateSessionID = () => {
-    return require("crypto").randomBytes(6).toString("hex");
+function generateSessionID(): string {
+    return crypto.randomBytes(6).toString("hex");
 }
+
+export { base64decode, base64encode, pbkdf2, generateSalt, generateSessionID };
