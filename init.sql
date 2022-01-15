bd918208154d424e7a469f79c4816ee891f1f16e
Bram <amogus@sus.com>
2022-01-15 18:28:37 +0100

    update init.sql to just 1 table for login info

--- init.sql
+++ init.sql
@@ -1,17 +1,10 @@
--- Create the databases
-CREATE DATABASE cheesepics_main;
-CREATE DATABASE cheesepics_salt;
+-- Create the database
+CREATE DATABASE cheesepics;
 
--- Create the main table
+-- Create the login table
 USE cheesepics_main;
-CREATE TABLE `main` (
+CREATE TABLE `login` (
     `username` TEXT,
-    `password` CHAR(128)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Create the salt table
-USE cheesepics_salt;
-CREATE TABLE `salt` (
-    `username` TEXT,
+    `password` CHAR(128),
     `salt` TEXT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
