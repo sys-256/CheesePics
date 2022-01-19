fdd69f4e28177d2980cc37fbc50b59be1432c6a9
Bram <amogus@sus.com>
2022-01-19 09:30:22 +0100

    init.sql: add liked column + fix error

--- init.sql
+++ init.sql
@@ -2,9 +2,10 @@
 CREATE DATABASE cheesepics;
 
 -- Create the login table
-USE cheesepics_main;
+USE cheesepics;
 CREATE TABLE `login` (
     `username` TEXT NOT NULL,
     `password` CHAR(128) NOT NULL,
-    `salt` TEXT NOT NULL
+    `salt` TEXT NOT NULL,
+    `liked` TEXT DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
