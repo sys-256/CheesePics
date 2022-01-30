6147dda38273cce62ef174bde3efb9419c7c0360
Bram <amogus@sus.com>
2022-01-30 20:02:46 +0100

    websocket.ts: add upload avatar ability

--- src/websocket.ts
+++ src/websocket.ts
@@ -10,6 +10,7 @@ import { WebSocketServer } from 'ws'
 import * as helper from "./helper";
 import { login } from "./websocket_functions/login.js";
 import { register } from "./websocket_functions/register.js";
+import { uploadAvatar } from "./websocket_functions/uploadAvatar.js";
 
 export const startWSServer = () => {
     const ws = new WebSocketServer({ port: config.port.websocket }, async () => {
@@ -40,17 +41,28 @@ export const startWSServer = () => {
                     socket.close();
                     return;
                 }
-                const new_message = message_encrypted.slice(message_encrypted.indexOf("::") + 2);
+                const new_message: string = message_encrypted.slice(message_encrypted.indexOf("::") + 2);
 
                 // Make sure it's a valid encrypted message
-                if (new_message.length !== 128) {
-                    socket.send(`ERR;;CLIENT;;Invalid message length.`);
-                    socket.close();
-                    return;
+                let pfp_data: string | undefined;
+                if (new_message.length !== 128 && new_message.includes("::")) {
+                    if (new_message.includes("::")) {
+                        try {
+                            pfp_data = new_message.slice(new_message.indexOf("::") + 2);
+                        } catch (err) {
+                            socket.send(`ERR;;CLIENT;;Please send a valid encrypted message.`);
+                            socket.close();
+                            return;
+                        }
+                    } else {
+                        socket.send(`ERR;;CLIENT;;Invalid message length.`);
+                        socket.close();
+                        return;
+                    }
                 }
 
                 // Decrypt the message with the private key and split it into parts by ;;
-                const message = keypair.privateKey.decrypt(new_message).split(";;");
+                const message: string[] = keypair.privateKey.decrypt(new_message.split("::")[0]).split(";;");
 
                 // Check if the message contains at least 2 parts
                 if (message.length < 1) {
@@ -78,6 +90,16 @@ export const startWSServer = () => {
                     }
 
                     login(socket, message, clientPublickey);
+                }
+                if (message[0] === "ICON" && pfp_data) {
+                    // Check if the message enough info
+                    if (message.length !== 2) {
+                        socket.send(`ERR;;CLIENT;;Specify (only) a method, username and password.`);
+                        socket.close();
+                        return;
+                    }
+
+                    uploadAvatar(socket, message, clientPublickey, pfp_data);
                 }
             });
         });
