e8d383ba0923aff0de49c0df6a1e5e4ad905ac15
Bram <amogus@sus.com>
2022-01-30 20:00:43 +0100

    init.sql: add pfp column

--- init.sql
+++ init.sql
@@ -7,5 +7,6 @@ CREATE TABLE `login` (
     `username` TEXT NOT NULL,
     `password` CHAR(128) NOT NULL,
     `salt` TEXT NOT NULL,
-    `liked` TEXT DEFAULT NULL
+    `liked` TEXT DEFAULT NULL,
+    `pfp` CHAR(68) DEFAULT NULL, -- 64 for sha256 hash + 4 for .png
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
