f9b3940f5f15cab46a801070a6924676984165d0
Bram <amogus@sus.com>
2022-01-31 20:57:04 +0100

    remove memcache.ts as it's unused

--- src/helpers/memcache.ts
+++
@@ -1,7 +0,0 @@
-// Get variables from config.json
-import { config } from "../../config.js";
-
-// Import packages
-import forge from 'node-forge';
-import Memcached from "memcached";
-const memcached = new Memcached(`${config.memcached.url}:${config.memcached.port}`);
