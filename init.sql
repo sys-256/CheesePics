91115115afd39560048729590d83ab07e36de7b9
Bram <amogus@sus.com>
2022-02-05 15:46:29 +0100

    init.sql: add images table

--- init.sql
+++ init.sql
@@ -1,12 +1,19 @@
 -- Create the database
 CREATE DATABASE cheesepics;
+USE cheesepics;
 
 -- Create the login table
-USE cheesepics;
 CREATE TABLE `login` (
     `username` TEXT NOT NULL,
     `password` CHAR(128) NOT NULL,
     `salt` TEXT NOT NULL,
     `liked` TEXT DEFAULT NULL,
-    `pfp` CHAR(68) DEFAULT NULL, -- 64 for sha256 hash + 4 for .png
+    `pfp` CHAR(68) DEFAULT NULL, -- 64 for sha256 hash + 4 for extension
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Create the images table
+CREATE TABLE `images` (
+    `ID`	    CHAR(36), -- 32 for the hex UUID + 4 for .jpg
+	`license`	TEXT,
+	`author`	TEXT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
